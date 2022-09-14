24b3456487087fe4662e85600bcdaced456fefec
sujit <sujitbokade2014@gmail.com>
2022-09-14 19:32:27 +0530

    [Sujit]Create Board

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game!");
+        char[] board = new char[10];
+        createBoard(board);
+    }
+    static void createBoard(char[] board) {
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+}
